885458c156db4910f51923e6c46ae3d845787a6f
Rishabh-Kasaudhan <rishabhgupta1974@gmail.com>
2026-03-15 21:03:56 +0530

    some commits

--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@ import connectDB from "./db/db.js";
 dotenv.config({
     path: "./env"
 });
-connectDB();
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("Error connecting to MongoDB:", err);
+});
